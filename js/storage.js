302d5ced353ad6b2cc4060d86f02155928fe7bcb
mrtarikozturk <mr.tarik.ozturk@gmail.com>
2020-10-17 22:41:09 +0200

    Add Storage class and its functions

---
+++ js/storage.js
@@ -0,0 +1,25 @@
+class Storage {
+    static get() {
+        let users;
+        if (localStorage.getItem('users') === null) users = [];
+        else users = JSON.parse(localStorage.getItem('users'));
+
+        return users;
+
+    }
+
+    static add(userName) {
+        let users = this.get();
+        if (users.indexOf(userName) === -1) {
+            users.push(userName);
+            localStorage.setItem('users', JSON.stringify(users));
+            return true;
+        }
+
+    }
+
+    static clear() {
+
+        localStorage.removeItem('users');
+    }
+}
